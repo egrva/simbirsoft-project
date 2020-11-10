b014e6ab987b278c6c8ce3e67e69a62b2aa6cd43
egrva <egorowanastena@mail.ru>
2020-11-10 15:45:29 +0300

    initial commit

---
+++ src/main/java/ru/aegorova/simbirsoftproject/SimbirsoftProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.aegorova.simbirsoftproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimbirsoftProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimbirsoftProjectApplication.class, args);
+    }
+
+}
